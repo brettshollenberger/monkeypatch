2fc453edf2187a8646283a310bef957e296e366f
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-07-09 17:59:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monkeypatch::Application.config.secret_token = '28f323c15f6f0bea2f7bc129c847305f8509ba21ad713bbcdc2daed4a0a8303688e64f62a677618095715cb6ce32f1df99bf81c3f0c6e43b95d28dcdcb7b1355'
